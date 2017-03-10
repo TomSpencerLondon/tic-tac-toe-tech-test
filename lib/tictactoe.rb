1e09e08ac53a21a8d1fe3dac6938486f0f79ac17
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-03-10 10:58:16 +0000

    tests and code for two player game for slot 0 and slot 3

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -4,9 +4,17 @@ class TicTacToe
 
   def initialize()
     @board = [nil, nil, nil, nil, nil, nil, nil, nil, nil]
+    @player = true
   end
 
   def play(slot=0)
-    @board[slot] = true
+    @board[slot] = @player
+    next_player
+  end
+
+  private
+
+  def next_player
+    @player = !@player
   end
 end
--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -11,9 +11,20 @@ describe TicTacToe do
     end
 
     describe "#play" do
-      it "should allow user to play counter in a slot" do
+      it "should allow player 1 to play counter in slot 0" do
         tictactoe.play(0)
         expect(tictactoe.board).to eq([true, nil, nil, nil, nil, nil, nil, nil, nil])
+      end
+
+      it "should allow player 1 to play counter in slot 3" do
+        tictactoe.play(3)
+        expect(tictactoe.board).to eq([nil, nil, nil, true, nil, nil, nil, nil, nil])
+      end
+
+      it "should allow player 1 to play counter in slot 0 and player 2 in slot 3" do
+        tictactoe.play(0)
+        tictactoe.play(3)
+        expect(tictactoe.board).to eq([true, nil, nil, false, nil, nil, nil, nil, nil])
       end
     end
   end
